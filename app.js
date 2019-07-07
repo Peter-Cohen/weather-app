474fa927f45a285c6de68018d3f749b694b96ac5
Peter-Cohen <peter.cohen.1969@gmail.com>
2019-07-07 23:45:40 +0800

    First commit
    First draft of the homepage

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('home.ejs');
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
---
+++ public/scripts/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let elems = document.querySelectorAll('.sidenav');
+  let instances = M.Sidenav.init(elems);
+});
+
+var dropdowns = document.querySelectorAll('.dropdown-trigger')
+for (var i = 0; i < dropdowns.length; i++){
+	M.Dropdown.init(dropdowns[i]);
+}
