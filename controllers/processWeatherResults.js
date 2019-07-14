e0bbbe7e807cb2b85a0ca07be2e98a954b519930
Peter-Cohen <peter.cohen.1969@gmail.com>
2019-07-14 21:41:53 +0800

    Refactored "hourly" part  in processWeatherResults.js

--- controllers/processWeatherResults.js
+++ controllers/processWeatherResults.js
@@ -8,27 +8,34 @@ const processWeatherResults = (apiResponse) => {
     time: formatTime(apiResponse.data.timezone,
       new Date(apiResponse.data.currently.time * 1000)),
     summary: apiResponse.data.currently.summary,
-    temperature: apiResponse.data.currently.temperature,
-    apparentTemperature: apiResponse.data.currently.apparentTemperature,
-    humidity: apiResponse.data.currently.humidity * 100,
+    temperature: `${apiResponse.data.currently.temperature} \u2103`,
+    apparentTemperature: `${apiResponse.data.currently.apparentTemperature} \u2103`,
+    humidity: `${apiResponse.data.currently.humidity * 100} %`,
     uvIndex: apiResponse.data.currently.uvIndex,
-    pressure: apiResponse.data.currently.pressure,
+    pressure: `${apiResponse.data.currently.pressure} \u33D4`,
   };
 
 
   // Hourly
-  const hourly = [1, 3, 6, 9, 12, 18, 24];
-  const hourlyWantedData = ['temperature', 'precipIntensity', 'precipProbability'];
+  const whichHours = [1, 3, 6, 9, 12, 18, 24];
+  const hourlyWhichDataPoints = ['temperature', 'precipProbability', 'precipIntensity'];
   const hourlyResults = {
     summary: apiResponse.data.hourly.summary,
     data: {},
   };
 
-  hourly.forEach((e) => {
+  whichHours.forEach((e) => {
     hourlyResults.data[e] = {};
-    hourlyWantedData.forEach((f) => {
-      hourlyResults.data[e][f] = apiResponse.data.hourly.data[e][f];
-    });
+    
+    // hourlyWhichDataPoints.forEach((f) => {
+    //   hourlyResults.data[e][f] = apiResponse.data.hourly.data[e][f];
+    // });
+
+    hourlyResults.data[e].Temperature = `${Math.round(apiResponse.data.hourly.data[e].temperature * 10) / 10}\u2103`;
+    hourlyResults.data[e]['Precip chance'] = `${Math.round(apiResponse.data.hourly.data[e].precipProbability * 100 * 10) / 10}%`;
+    hourlyResults.data[e]['Precip mm'] = Math.round(apiResponse.data.hourly.data[e].precipIntensity * 10) / 10;
+    hourlyResults.data[e]['Precip type'] = apiResponse.data.hourly.data[e].precipType || '-';
+    
   });
 
 
