c640c82e113d71c235083f8a9bd7df3df5d71059
Peter-Cohen <peter.cohen.1969@gmail.com>
2019-07-15 23:01:31 +0800

    Try fix links in dropdown on mobile not working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,11 +1,11 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   let elems = document.querySelectorAll('.sidenav');
-//   let instances = M.Sidenav.init(elems);
-// });
+document.addEventListener('DOMContentLoaded', () => {
+  let elems = document.querySelectorAll('.sidenav');
+  let instances = M.Sidenav.init(elems);
+});
 
-// var dropdowns = document.querySelectorAll('.dropdown-trigger')
-// for (var i = 0; i < dropdowns.length; i++){
-// 	M.Dropdown.init(dropdowns[i]);
-// }
+var dropdowns = document.querySelectorAll('.dropdown-trigger')
+for (var i = 0; i < dropdowns.length; i++){
+	M.Dropdown.init(dropdowns[i]);
+}
 
-M.AutoInit();
+// M.AutoInit();
